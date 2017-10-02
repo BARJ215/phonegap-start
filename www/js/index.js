2e72f3db00014bc05d1d59d48906894adccecf36
BARJ215 <32267193+BARJ215@users.noreply.github.com>
2017-10-02 19:40:37 +0100

    Info Fix 6

--- www/js/index.js
+++ www/js/index.js
@@ -34,10 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		//The following code found from https://www.youtube.com/watch?v=sSr1s19gND8
-		$("#info").html('Cordova Version: ' +device.cordova+'<br>');
-		$("#info").append(device.model+'<br>');
-		$("#info").append(device.platform+' '+device.version +'<br>');
 		
     },
     // Update DOM on a Received Event
@@ -48,6 +44,11 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+		
+		//The following code found from https://www.youtube.com/watch?v=sSr1s19gND8
+		$("#info").html('Cordova Version: ' +device.cordova+'<br>');
+		$("#info").append(device.model+'<br>');
+		$("#info").append(device.platform+' '+device.version +'<br>');
 
         console.log('Received Event: ' + id);
     }
